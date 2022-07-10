deffab42457b8551cc7030749dbadd9737fd08b4
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-07-10 13:04:54 +0000

    added MultiPolygon to model

--- main/models.py
+++ main/models.py
@@ -15,7 +15,10 @@ class Plot(models.Model):
     zone = models.CharField(max_length=50)
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,13}$', message="Phone number must be entered in the format: '+999999999'. Up to 13 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # Validators should be a list
-    plot = models.PolygonField()
-
-   
-   
+    plot = models.PolygonField() #! will remove this later
+    geom = models.MultiPolygonField(null=True)
+    class Meta:
+        verbose_name = "Plot"
+        verbose_name_plural = "Plots"
+    def __str__(self):
+        return f'{ self.owner_name } { self.geom }'
