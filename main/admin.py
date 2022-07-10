e677bb637a04a55698c2e858a545554506be2051
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-07-10 14:27:52 +0000

    updated with LeafletGeoAdmin

--- main/admin.py
+++ main/admin.py
@@ -1,11 +1,14 @@
-from django.contrib import admin
-from django.contrib.gis.db import models
-from django.contrib.gis.admin import OSMGeoAdmin
+# from django.contrib import admin
+# from django.contrib.gis.db import models
+# from django.contrib.gis.admin import OSMGeoAdmin
+
+from django.contrib.gis import admin
+from leaflet.admin import LeafletGeoAdmin
 from .models import Plot
 
 
 @admin.register(Plot)
-class PlotAdmin(OSMGeoAdmin):
+class PlotAdmin(LeafletGeoAdmin):
     list_display = ('owner_name', 'zone', 'phone_number')
 
 class PlotAdmin(admin.ModelAdmin):
