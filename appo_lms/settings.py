ea7ab59ef3427b143dbba8c866de30ae85e66070
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-07-18 15:00:29 +0000

    MEDIA AND STATIC SETTINGS

--- appo_lms/settings.py
+++ appo_lms/settings.py
@@ -172,7 +172,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 GOOGLE_API_KEY = "AIzaSyBf4mB4ZfsTmvktmiT_aCIqbQUcpJpgJSs"
 RECAPTCHA_KEY = "6LcD1AQdAAAAAMRxmWIlqSH8xBUHRh_qFP_-G0P3"
