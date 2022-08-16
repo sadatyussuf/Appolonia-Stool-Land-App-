6e2c7aab7bfaa9aff3834d8c019315c02ea8dec3
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-08-16 12:12:53 +0000

    read csv

--- main/views.py
+++ main/views.py
@@ -1,45 +1,43 @@
-# from cProfile import label
-# from multiprocessing import context
-# from pickletools import long4
-# from django import forms
-from django.http import HttpResponse, HttpResponseRedirect
+
 from django.shortcuts import render, redirect
 from .models import Plot
-# from django.contrib.auth.forms import UserCreationForm
 from .forms import PlotForm
 
-# ****************************
-# from django.views import generic
-# from django.contrib.gis.geos import fromstr,Point
-# from django.core.serializers import serialize
-
+from csv import DictReader
+from io import TextIOWrapper
 # longitude = -80.191788
-# latitude = 25.761681            5.796733790344598, -0.07945842216120023
+# latitude = 25.761681     
 
-# my_location = Point(longitude, latitude, srid=4326)  +2330548030734 
+# my_location = Point(longitude, latitude, srid=4326)  
 
       
 # # Create your views here.
 def index(request):
-    # print('............................')
     model_plots = Plot.objects.all()
-    # form = PlotForm(request.POST, request.FILES)
+
+    return render(request, 'templates/main/index.html',{'plots':model_plots})
+
+
+def create_plot(request):
+    if request.method == "POST":
+        form = PlotForm(request.POST or None, request.FILES or None)
+
+        test = request.FILES['file_upload']
+        rows = TextIOWrapper(test,encoding='utf-8',newline="")
+        for row in DictReader(rows):
+            print(row)
+ 
+
+    else:
+        form = PlotForm()
+    return render(request, 'templates/main/newplot.html',{"form":form})
+
+
+
+   # form = PlotForm(request.POST, request.FILES)
     # if request.method == 'POST':
     #     form = PlotForm(request.POST, request.FILES)
     #     print('............................')
     #     print('Request Is POST')
     #     if form.is_valid():
     #         form.save()
-    return render(request, 'templates/main/index.html',{'plots':model_plots})
-
-
-
-def create_plot(request):
-    if request.method == "POST":
-        form = PlotForm(request.POST or None, request.FILES or None)
-        test = request.FILES['file_upload']
-        print(test.read())
-
-    else:
-        form = PlotForm()
-    return render(request, 'templates/main/newplot.html',{"form":form})
