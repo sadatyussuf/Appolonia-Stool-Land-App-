32f666df87d7790961a6616075fad1f8b00be90a
Sadat Issah <51789921+sadatyussuf@users.noreply.github.com>
2022-08-17 09:35:48 +0000

    added setView to map layer

--- static/assets/js/map.js
+++ static/assets/js/map.js
@@ -1,4 +1,4 @@
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-const map = L.map('map')
+const map = L.map('map').setView([5.5645, -0.2257], 15)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: attribution }).addTo(map);
-map.fitWorld();
+// map.fitWorld();
